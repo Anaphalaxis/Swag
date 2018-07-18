887dff312319f17c6b27a824108068aa7d33aeb3
Anaphalaxis <whalepoopheaven@gmail.com>
2018-07-17 19:55:35 -0700

    Lesson-01

--- app/src/main/java/com/example/klai/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/klai/coderswag/Controller/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.klai.coderswag
+package com.example.klai.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.klai.coderswag.R
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +10,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
---
+++ app/src/main/java/com/example/klai/coderswag/Model/Product.kt
@@ -0,0 +1,3 @@
+package com.example.klai.coderswag.Model
+
+class Product(val title: String, val price, String, val image: String)
