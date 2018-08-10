1e8a0d61b22722cdb4949f36cb6668d0415bf45e
Kevin Lai <whalepoopheaven@gmail.com>
2018-08-09 18:05:37 -0700

    Middle of 15

--- app/src/main/java/com/example/klai/coderswag/Adapters/CategoryAdapter.kt
+++ app/src/main/java/com/example/klai/coderswag/Adapters/CategoryAdapter.kt
@@ -18,10 +18,19 @@ class CategoryAdapter(context: Context, categories: List<Category>) : BaseAdapte
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val categoryView: View
+        val holder : ViewHolder
 
-        categoryView = LayoutInflater.from(context).inflate(R.layout.category_list_item, null)
-        val categoryImage : ImageView = categoryView.findViewById(R.id.categoryImage)
-        var categoryName : TextView = categoryView.findViewById(R.id.categoryName)
+        if (convertView == null){
+            categoryView = LayoutInflater.from(context).inflate(R.layout.category_list_item, null)
+            holder = ViewHolder()
+            holder.categoryImage = categoryView.findViewById(R.id.categoryImage)
+            holder.categoryName = categoryView.findViewById(R.id.categoryName)
+
+            categoryView.tag = holder
+        }
+
+
+
 
         val category = categories[position]
 
@@ -44,5 +53,10 @@ class CategoryAdapter(context: Context, categories: List<Category>) : BaseAdapte
 
     override fun getCount(): Int {
         return categories.count()
+    }
+
+    private class ViewHolder {
+        var categoryImage: ImageView? = null
+        var categoryName: TextView? = null
     }
 }
